f900d42a8dfd8fa20cdcae1192ff880afd14da97
Arokiya-Kithiyon <arokiyakithiyon2016@gmail.com>
2024-11-06 09:59:09 +0530

    privacy policy updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,15 +14,15 @@ function Footer() {
       {privacy && (
         <div
           id="privacy-policy"
-          className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full text-white font-poppins border-2 px-4 py-4 rounded-lg`}
+          className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full text-white font-poppins border border-slate-700 px-4 py-4 rounded-lg`}
         >
           <PrivacyPolicy />
           <div className=" pr-3">
             <img
               title="Close"
               src={cross}
-              alt="cancel"
-              className=" hover:cursor-pointer w-52 "
+              alt="Cancel"
+              className=" hover:cursor-pointer w-28 "
               onClick={() => setPrivacy(!privacy)}
             />
           </div>
@@ -32,7 +32,7 @@ function Footer() {
       {condition && (
         <div
           id="terms-conditions"
-          className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full text-white font-poppins border-2 px-4 py-4 rounded-lg`}
+          className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full text-white font-poppins border border-slate-700 px-4 py-4 rounded-lg`}
         >
           <TermsAndConditions />
 
@@ -40,8 +40,8 @@ function Footer() {
             <img
               title="Close"
               src={cross}
-              alt="cancel"
-              className=" hover:cursor-pointer w-52 "
+              alt="Cancel"
+              className=" hover:cursor-pointer w-28 "
               onClick={() => setCondition(!condition)}
             />
           </div>
@@ -51,28 +51,25 @@ function Footer() {
       <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
         <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
           Copyright Ⓒ 2024 Brainz. All Rights Reserved.{" "}
-          <a onClick={() => setPrivacy(!privacy)} href="#privacy-policy">
+          <a
+            className="text-sky-200 ml-2"
+            onClick={() => setPrivacy(!privacy)}
+            href="#privacy-policy"
+          >
             Privacy Policy
           </a>
           {"  "} and{"  "}
-          <a onClick={() => setCondition(!condition)} href="#terms-conditions">
+          <a
+            className="text-sky-200 ml-2"
+            onClick={() => setCondition(!condition)}
+            href="#terms-conditions"
+          >
             Terms & Conditions
           </a>
         </p>
 
         <div className="flex flex-row md:mt-0 mt-6">
           <h3 className={`${styles.heading2} text-gradient`}>Brainz</h3>
-          {/* {socialMedia.map((social, index) => (
-            <img
-              key={social.id}
-              src={social.icon}
-              alt={social.id}
-              className={`w-[21px] h-[21px] object-contain cursor-pointer ${
-                index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
-              }`}
-              onClick={() => window.open(social.link)}
-            />
-          ))} */}
         </div>
       </div>
     </section>
